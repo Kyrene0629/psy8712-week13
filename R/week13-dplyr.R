b544bfe3ebafec09f97260d8c4d141c504342760
Kyrene Li <li003631@umn.edu>
2026-04-26 20:21:08 -0500

    create and save tables

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -1,6 +1,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
 library(DBI)
 library(RPostgres)
+library(tidyverse)
 
 con <- dbConnect(
   RPostgres::Postgres(),
@@ -11,3 +12,18 @@ con <- dbConnect(
   password = Sys.getenv("NEON_PW"),
   sslmode = "require"
 )
+
+# download the three SQL tables
+employees_tbl <- dbGetQuery(con, "SELECT * 
+                            FROM datascience_employees")
+testscores_tbl <- dbGetQuery(con, "SELECT * 
+                             FROM datascience_testscores")
+offices_tbl <- dbGetQuery(con, "SELECT * 
+                          FROM datascience_offices")
+
+# save the imported tables into the out subfolder
+write_csv(employees_tbl, "../out/employees.csv")
+write_csv(testscores_tbl, "../out/testscores.csv")
+write_csv(offices_tbl, "../out/offices.csv")
+
+
